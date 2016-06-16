835e74de9474b2a57ee03109408c2d56fc1949a6
Niral Pokal <niralpokal@gmail.com>
2016-06-16 12:22:22 -0700

    I displayed a welcome message fort the user. #3

---
+++ server/dist/js/components/HomePage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+class HomePage extends React.Component{
+
+  render(){
+    return(
+      <div className="container">
+        <div className="row-fluid">
+          <div className="col-md-offset-3 col-md-offset-6 text-center">
+            <h3>Welcome! </h3>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default HomePage
